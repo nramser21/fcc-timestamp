83c721b722d781b08bc24fc860653074ce5ba820
Nick <nramser21@googlemail.com>
2017-06-02 22:24:36 +0000

    Wrote timestamp service

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 var express = require('express');
 var app = express();
-app.get('/', (req, res) => {
-    res.end('Hello World!');
+
+var months = ['January', 'Febuary', 'March', 'April', 'May', 'June',
+              'July', 'August', 'September', 'October', 'November', 'December'];
+
+function unixToNatural(ms) {
+    var date = new Date();
+    date.setMilliseconds(ms);
+    var year = ', ' + date.getFullYear();
+    var month = months[date.getMonth()];
+    var day = ' ' + date.getDate();
+    return month + day + year;
+}
+
+app.get('/:query', (req, res) => {
+    var query = req.params.query;
+    var time = {
+        unix: null,
+        natural: null
+    };
+
+    var date = Date.parse(query);
+    if (!Number.isNaN(date)) {
+        console.log("Natural");
+        time.unix = date / 1000;
+        time.natural = query;
+    } else if (parseInt(query)) {
+        time.unix = parseInt(query);
+        time.natural = unixToNatural(time.unix);
+    } 
+    res.send(time);
 });
+
 app.listen(8080);
